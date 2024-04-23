029ea1a5daff6c06dfc52210f2a081c09e11d0b6
quadhier <quadhier@163.com>
2024-04-23 18:27:23 +0800

    Use unsafe

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,6 +1,8 @@
-pub struct List<'a, T> {
+use std::ptr;
+
+pub struct List<T> {
     head: Link<T>,
-    tail: Option<&'a mut Node<T>>,
+    tail: *mut Node<T>,
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -10,36 +12,37 @@ struct Node<T> {
     next: Link<T>,
 }
 
-impl<'a, T> List<'a, T> {
+impl<T> List<T> {
     pub fn new() -> Self {
-        List { head: None, tail: None }
+        List { head: None, tail: ptr::null_mut() }
     }
 
-    pub fn push(&'a mut self, elem: T) {
-        let new_tail = Box::new(Node {
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node {
             elem,
             next: None,
         });
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
+
+        let raw_tail: *mut _ = &mut *new_tail;
+
+        if !self.tail.is_null() {
+            unsafe {
+                (*self.tail).next = Some(new_tail);
             }
-            None => {
-                self.head= Some(new_tail);
-                self.head.as_deref_mut()
-            }
-        };
-        self.tail = new_tail;
+        } else {
+            self.head = Some(new_tail);
+        }
+
+        self.tail = raw_tail;
     }
 
-    pub fn pop(&'a mut self) -> Option<T> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|head| {
             let head = *head;
             self.head = head.next;
 
             if self.head.is_none() {
-                self.tail = None;
+                self.tail = ptr::null_mut();
             }
             head.elem
         })
@@ -52,25 +55,32 @@ mod test {
 
     #[test]
     fn basics() {
-        // let mut list = List::new();
-        //
-        // assert_eq!(list.pop(), None);
-        //
-        // // Populate list
-        // list.push(1);
-        // list.push(2);
-        // list.push(3);
-        //
-        // assert_eq!(list.pop(), Some(1));
-        // assert_eq!(list.pop(), Some(2));
-        //
-        // list.push(4);
-        // list.push(5);
-        //
-        // assert_eq!(list.pop(), Some(3));
-        // assert_eq!(list.pop(), Some(4));
-        //
-        // assert_eq!(list.pop(), Some(5));
-        // assert_eq!(list.pop(), None);
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        list.push(6);
+        list.push(7);
+
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
+        assert_eq!(list.pop(), None);
     }
 }
